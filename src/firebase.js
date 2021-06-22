c6923667b8cf1bee7b943905b1e42f15636be645
umarhammed <hammedumar007@gmail.com>
2021-06-22 13:17:19 +0100

    firebase installed

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDnzPPUg0J69ceovUtY3p1DAuq0V93TC0Y',
+  authDomain: 'clone-f26dd.firebaseapp.com',
+  projectId: 'clone-f26dd',
+  storageBucket: 'clone-f26dd.appspot.com',
+  messagingSenderId: '562962265552',
+  appId: '1:562962265552:web:e43638c8d8e2236cb875d5',
+  measurementId: 'G-B33TS7Q0GF',
+}
